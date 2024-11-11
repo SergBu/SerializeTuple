0713f6704ea16c6c7c8115079dbc328d18107c14
Serg Budnikov <sergei.budnikov@smartseeds.ru>
2024-11-11 16:48:13 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json;
+
+JsonSerializerOptions options = new() { IncludeFields = true };
+
+var testTuple = ("test", "test1", 1324, false);
+var serializedTuple = JsonSerializer.Serialize(testTuple, options);
+Console.WriteLine(serializedTuple);
+
+var testTuple2 = (NamedItem1: "test", NamedItemTwo: "test1", TheIntegersName: 1324, ThisBoolHasAFirstNameIts: false);
+var serializedTuple2 = JsonSerializer.Serialize(new { testTuple2.NamedItem1, testTuple2.NamedItemTwo, testTuple2.TheIntegersName, testTuple2.ThisBoolHasAFirstNameIts }, options);
+Console.WriteLine(serializedTuple2);
